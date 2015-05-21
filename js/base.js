e64a84bdb6fddaddb6865ccdfcc0f04064e59cb6
xeechou <xeechou@gmail.com>
2015-05-21 12:19:56 +0800

    first blog

--- js/base.js
+++ js/base.js
@@ -10,7 +10,11 @@ $('body').scrollspy({
     target: '.bs-sidebar',
 });
 
+
 /* Prevent disabled links from causing a page reload */
 $("li.disabled a").click(function() {
     event.preventDefault();
 });
+
+
+
